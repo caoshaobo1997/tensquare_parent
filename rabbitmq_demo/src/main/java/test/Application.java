1ae151e921c756c74ffe1f2f94a80f43941d4eba
shaobo <2568938671@qq.com>
2019-05-09 11:46:12 +0800

    修改

--- rabbitmq_demo/src/main/java/test/Application.java
+++ rabbitmq_demo/src/main/java/test/Application.java
@@ -3,9 +3,15 @@ package test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author 曹少博
+ */
 @SpringBootApplication
 public class Application {
     public static void main(String[] args){
         SpringApplication.run(Application.class);
     }
+
+    //test Git pull
+
 }
